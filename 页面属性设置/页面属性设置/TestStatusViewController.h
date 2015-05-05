f7faafffa4ec9e8a702c5134d225b3d8b5da4e19
陶照平 <22292436@qq.com>
2015-05-05 17:24:21 +0800

    更新

---
+++ 页面属性设置/页面属性设置/TestStatusViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestStatusViewController.h
+//  页面属性设置
+//
+//  Created by 尹亚涛 on 15/5/5.
+//  Copyright (c) 2015年 taozhaoping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestStatusViewController : UIViewController
+
+@end
---
+++ 页面属性设置/页面属性设置/TestStatusViewController.m
@@ -0,0 +1,67 @@
+//
+//  TestStatusViewController.m
+//  页面属性设置
+//
+//  Created by 尹亚涛 on 15/5/5.
+//  Copyright (c) 2015年 taozhaoping. All rights reserved.
+//
+
+#import "TestStatusViewController.h"
+
+@interface TestStatusViewController ()
+
+@property (nonatomic,strong) NSAttributedString* testToAnalyze;
+@property (weak, nonatomic) IBOutlet UILabel *charateWidthStatus;
+@property (weak, nonatomic) IBOutlet UILabel *charateColorStatus;
+@end
+
+@implementation TestStatusViewController
+
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    self.testToAnalyze = [[NSAttributedString alloc] initWithString:@"sdjfksjfksfjsdkfjdsk"];
+}
+
+-(void)setTestToAnalyze:(NSAttributedString *)testToAnalyze
+{
+    _testToAnalyze = testToAnalyze;
+    //不在当前视图更新文本的时候交由viewWillAPPEART进行gengxin
+    if(self.view.window )[self updateUI];
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self updateUI];
+}
+
+-(NSAttributedString *)charaterWithAttributed:(NSString *)attributedName
+{
+    NSMutableAttributedString* charater = [[NSMutableAttributedString alloc] init];
+    NSUInteger index = 0;
+    while (index < [self.testToAnalyze length]) {
+        NSRange range;
+        id value = [self.testToAnalyze attribute:attributedName atIndex:index effectiveRange:&range];
+        if (value) {
+            [charater appendAttributedString:[self.testToAnalyze attributedSubstringFromRange:range]];
+            index = range.location + range.length;
+        }else
+        {
+            index++;
+        }
+    }
+    return charater;
+}
+
+-(void)updateUI
+{
+    NSAttributedString* charateWidth = [self charaterWithAttributed:NSStrokeWidthAttributeName];
+    NSAttributedString* charateColor = [self charaterWithAttributed:NSForegroundColorAttributeName];
+    
+     self.charateWidthStatus.text = [NSString stringWithFormat:@"%zi:颜色的字符串",[charateWidth length]];
+    self.charateColorStatus.text = [NSString stringWithFormat:@"%zi:颜色的字符串", [charateColor length]];
+                                    
+}
+
+@end
